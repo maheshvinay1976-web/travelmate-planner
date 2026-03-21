f87562f2c76fa9b3df5c7c4b51429a25e8927484
maheshvinay1976-web <maheshvinay1976@gmail.com>
2026-03-21 10:38:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -192,5 +192,7 @@ def delete_trip(id):
 
 
 # ✅ RUN APP (FOR LOCAL ONLY)
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
